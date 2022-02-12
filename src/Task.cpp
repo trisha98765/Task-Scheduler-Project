488b6e8d4bf850baf9f5b223c606ad0681781c2b
melle-issa <mhida010@ucr.edu>
2022-02-12 10:54:37 -0800

    implemented print function

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Task.h"
 
 using namespace std;
+
+void Task::print(){
+    cout << "Name: " << this->getName() << endl;
+    cout << "Due: " << this->getDueDate() << " Priority: " << this->getPriority() << endl;
+    cout << "Classification: " << this-> getClassification() << "Duration: " << this->getDuration() << endl;
+    cout << "Description: " << this-> getDescription() << endl;
+}
 
 void Task::delete() {
     // TODO
