2437c46bd834be49923e68a18f726a61d40fc045
Melissa Hidalgo <74077340+melle-issa@users.noreply.github.com>
2022-02-12 18:01:48 -0800

    made print task list function

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <vector>
 #include "TaskList.h"
+#include "Task.h"
 
 using namespace std;
+
+void TaskList::print(){
+    int size = this->size();
+    for(int i = 0; i < size; i++){
+        this.at(i)->print(); //don't know if this is correct or not   
+    }
+}
 
 void TaskList::delete() {
     // TODO
