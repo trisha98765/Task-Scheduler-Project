038affc0f13333df60a9ad5ed748924fb132f8fb
trisha112 <tagra002@ucr.edu>
2022-02-13 22:00:38 -0800

    added setters

--- header/Task.h
+++ header/Task.h
@@ -1,11 +1,12 @@
+#ifndef TASK_H
+#define TASK_H
+
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "Named.h"
 #include "Deletable.h"
-
-#ifndef TASK_H
-#define TASK_H
 
 using namespace std;
 
@@ -16,23 +17,31 @@ class Task : public Named, public Deletable {
         int priority;
         string classification;
         int duration;
+        string dueDate; // we can change the type to int if thats better
         bool completed;
         int inProgress;
     public:
-        void setName();
-        void setDescription();
-        void setPriority();
-        void setDuration();
-        void setDueDate();
-        void setClassification();
+        void setName(string n);
+        void setDescription(string d);
+        void setPriority(int val);
+        void setDuration(int dur);
+        void setDueDate(string date);
+        void setClassification(int label);
+        void setCompletion(bool check); // not sure if we want this one
+        void setProgress(int progress); // not sure about this either
+
         string getName();
         string getDescription();
         int getPriority();
-        string getClassification();
         int getDuration();
+        string getDueDate(); // the return val can also be a int ?
+        string getClassification();
+        bool getCompletion(); // not sure
+        int getProgress();    // not sure
+
         void print();
         void edit(); //specifications not complete
-        void delete();
+        void deleteTask(); // changed method name because 'delete' is c++ key word
 };
 
 #endif
--- src/Task.cpp
+++ src/Task.cpp
@@ -5,6 +5,38 @@
 
 using namespace std;
 
+void Task::setName(string n){
+    name = n;
+}
+
+void Task::setDescription(string d){
+    description = d;
+}
+
+void Task::setPriority(int val){
+    priority = val;
+}
+
+void Task::setDuration(int dur){
+    duration = dur;
+}
+
+void Task::setDueDate(string date){
+    dueDate = date;
+}
+
+void Task::setClassification(int label){
+    classification = label;
+}
+
+void Task::setCompletion(bool check){
+    completed = check;
+}
+
+void Task::setProgress(int progress){
+    inProgress = progress;
+}
+
 void Task::print(){
     cout << "Name: " << this->getName() << endl;
     cout << "Due: " << this->getDueDate() << " Priority: " << this->getPriority() << endl;
