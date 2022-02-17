67611d475c3a21dffb06b6ce3de452d9f5fdf63e
melle-issa <mhida010@ucr.edu>
2022-02-17 11:26:44 -0800

    created 1 test

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,13 @@
 #include "gtest/gtest.h"
+#include "Task.h"
+#include "TaskList.h"
+
+TEST(SetterTest, basic){
+    Task *test = new Task();
+    test->setName("name");
+    EXPECT_EQ(test->getName(), "name");
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
