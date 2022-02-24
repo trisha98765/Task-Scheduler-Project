685ab711cd19de5ee47a33969b40cf8a771c9131
trisha112 <tagra002@ucr.edu>
2022-02-23 22:15:11 -0800

    editing files

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 #include <vector>
-#include "TaskList.h"
-#include "Task.h"
+#include "../header/TaskList.h"
+#include "../header/Task.h"
 
 using namespace std;
 
-void TaskList::print(){
-    int size = this->size();
+void TaskList::print(std::ostream &out){ // i think this argument should be a Goal object so it can print a task or taskList object
+  // this does not compile because 'this' is not pointing at the vector
+    /* int size = this.size();
     for(int i = 0; i < size; i++){
-        this.at(i)->print(); //don't know if this is correct or not   
+       // this.at(i)->print(); //don't know if this is correct or not   
     }
+   */
 }
 
-void TaskList::deleteTaskList() {
+void TaskList::edit(){
     // TODO
 }
-void TaskList::deleteTask(int index2;) {
+/*
+void TaskList::deleteObj() {
+    // TODO
+    // to delete a task list that should happen in the task list class
+}
+*/
+Task TaskList::findTask(){
+    
+
+}
+
+/* to delete an individual task, to work with the composite pattern it needs to be a part of the task class because deleteObj is a pure virtual function to be redefined in both child classes, we can pass the vector of tasks as an argument to get access to the list*/
+void TaskList::deleteObj(int index2) {
     std::string input;
     int index = index2-1; //bc user enters 1 but index is 0
     std::cout << "Would you like to delete this task? Y/N" << std::endl;
     cin >> input;
-    if(input == 'Y'){
+    if(input == "Y"){
         std::cout << "Task deleted. Undo? Y/N" << std::endl;
         cin >> input;
-        if(input == 'Y'){
+        if(input == "Y"){
             std::cout << "Task restored" << std::endl;    
         }
-        else{
+        // the else statement doesnt compile because 'this' is not pointing at the vector
+       /* else{
             for(int i =index; i < this->size()-1; i++){
                 this->at(i) = this->at(i+1);
             }
             this->pop_back();
-        }
+        }*/ 
     }
     else{
         std::cout << "Task was not deleted" << std::endl;    
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "../header/TaskList.h"
 #include "../header/Task.h"
+#include "../header/Goal.h"
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 int main(){
- 
-  return 0;
+    // Goal vector<Task>
+    // call menu function from separate cpp file
+    //    
+    return 0;
 }
