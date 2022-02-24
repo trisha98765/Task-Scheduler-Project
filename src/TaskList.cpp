de4e78dcf2cd8959582d3898db270180fa3592b9
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-23 18:41:38 -0800

    Added delete function

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -15,3 +15,25 @@ void TaskList::print(){
 void TaskList::deleteTaskList() {
     // TODO
 }
+void TaskList::deleteTask(int index2;) {
+    std::string input;
+    int index = index2-1; //bc user enters 1 but index is 0
+    std::cout << "Would you like to delete this task? Y/N" << std::endl;
+    cin >> input;
+    if(input == 'Y'){
+        std::cout << "Task deleted. Undo? Y/N" << std::endl;
+        cin >> input;
+        if(input == 'Y'){
+            std::cout << "Task restored" << std::endl;    
+        }
+        else{
+            for(int i =index; i < this->size()-1; i++){
+                this->at(i) = this->at(i+1);
+            }
+            this->pop_back();
+        }
+    }
+    else{
+        std::cout << "Task was not deleted" << std::endl;    
+    }
+}
