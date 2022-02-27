b31d6ee430bbdd00249ac7028fab6adcd4b7f98b
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-27 15:58:49 -0800

    Update TaskList.cpp

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
 #include <vector>
-#include "../header/TaskList.h"
-#include "../header/Task.h"
+#include "TaskList.h" //change these back to ../header/
+#include "Task.h"
+#include "Goal.h"
 
 using namespace std;
 
-void TaskList::print(std::ostream &out){ // i think this argument should be a Goal object so it can print a task or taskList object
+TaskList::TaskList(){
+
+}
+
+void TaskList::print(){ // i think this argument should be a Goal object so it can print a task or TaskList object
   // this does not compile because 'this' is not pointing at the vector
-    /* int size = this.size();
+    int size = listObj.size();
     for(int i = 0; i < size; i++){
-       // this.at(i)->print(); //don't know if this is correct or not   
+       listObj.at(i)->print(); //don't know if this is correct or not   
     }
-   */
+   
 }
 
 
 void TaskList::edit(){
     // TODO
 }
-void TaskList::deleteObj(int index, vector<TaskList> &temp ) {
-    std::string input;
-    std::cout << "Would you like to delete this list? Y?N" << std::endl;
-    std::cin >> input;
-    if(input == "Y"){
-	std::cout << "List deleted. Undo? Y?N" << std::endl;
-	if(input == "Y"){
-	    std::cout << "List restored" << std::endl;
-	}
-	else{
-	    temp.at(index).erase();
-	}
-    }
+void TaskList::addTask(Goal *newOne){
+    listObj.push_back(newOne);
+}
+// void TaskList::deleteObj(int index, TaskList &tempList) {
+//     std::string input;
+//     std::cout << "Would you like to delete this list? Y?N" << std::endl;
+//     std::cin >> input;
+//     if(input == "Y"){
+//         std::cout << "List deleted. Undo? Y?N" << std::endl;
+//         if(input == "Y"){
+//             std::cout << "List restored" << std::endl;
+//         }
+//         else{
+//             //temp.at(index).erase();
+//         }
+//     }
+// }
+
+vector<Goal*>& TaskList::getList(){
+    return listObj;
 }
 
-Task TaskList::findTask(){
-   Task temp = new Task();
-    return temp; //just temporary for testing
+Goal* TaskList::findTask(string tempName){ //what if you didn't find anything?
+    vector<Goal*> tempVec = this->getList();
+    Goal *tempTask = tempVec.at(0);
+
+    std::string tempName2 = tempTask->getName();
+    int i = 0;
+
+    while(tempName2 != tempName){
+        i++;
+        tempTask = tempVec.at(i);
+        tempName2 = tempTask->getName();
+    }
+    
+    return tempVec.at(i);
+}
+
+int TaskList::findIndex(string tempName){
+    vector<Goal*> tempVec = this->getList();
+    Goal *tempTask = tempVec.at(0);
+
+    std::string tempName2 = tempTask->getName();
+    int i = 0;
+
+    while(tempName2 != tempName){
+        i++;
+        tempTask = tempVec.at(i);
+        tempName2 = tempTask->getName();
+    }
+    return i;
 }
