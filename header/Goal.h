151054c5fb35acdc4d02b18af35de14ccf8a1d75
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-27 15:55:50 -0800

    Update Goal.h

--- header/Goal.h
+++ header/Goal.h
@@ -4,23 +4,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include "TaskList.h"
+#include "Task.h"
 
 using namespace std;
 
-class Goal {
+class Goal{
     private:
         string name;
     public:
+        Goal();
+        Goal(string s){
+            name = s;
+        }
         void setName(string s){
             name = s;
         }
         string getName(){
             return name;
         }
-        virtual void print(std::ostream &out) = 0;
+        virtual void print() = 0; //had this as an argument: std::ostream &out
         virtual void edit() = 0;      // does this take arguments?
-        virtual void deleteObj(int index) = 0;
 };
 
 #endif
