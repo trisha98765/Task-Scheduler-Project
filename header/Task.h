c4c0ccc3ddc7e47f3b5184a2dc4a3ad1f685a3ff
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-27 15:56:34 -0800

    Update Task.h

--- header/Task.h
+++ header/Task.h
@@ -4,46 +4,64 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "TaskList.h"
 #include "Goal.h"
 
-using namespace std;
-
-class Task: public Goal {
+class Task: public Goal{
     private:
-        string name;
-        string description;
+        std::string name;
+        std::string description;
         int priority;
-        string classification;
+        std::string classification;
         int duration;
-        string dueDate;
+        std::string dueDate;
         bool completed;
         int inProgress;
     public:
-        Task();
-        Task(string n, string d, int val, int dur, string date, string label, bool check, int progress);
-        void setName(string n);
-        void setDescription(string d);
+        Task(){
+            name = "Homework";
+            description = "For CS 100";
+            priority = 1; // assuming 1 is the highest priority
+            classification = "School";
+            duration = 7; // number of days
+            dueDate = "03/12/22"; // we might have to validate input by user for this
+            completed = false;
+            inProgress = 5; // on a scale of 1-10, 5 is half progress made?
+        }
+        Task(std::string n, std::string d, int val, int dur, std::string date, std::string label, bool check, int progress){
+            name = n;
+            description = d;
+            priority = val;
+            duration = dur;
+            dueDate = date;
+            classification = label;
+            completed = check;      
+            inProgress = progress;
+        }
+        void setName(std::string n);
+        void setDescription(std::string d);
         void setPriority(int val);
         void setDuration(int dur);
-        void setDueDate(string date);
-        void setClassification(string label);
+        void setDueDate(std::string date);
+        void setClassification(std::string label);
         void setCompletion(bool check);
         void setProgress(int progress);
 
 
-        string getName();
-        string getDescription();
+        std::string getName();
+        std::string getDescription();
         int getPriority();
         int getDuration();
-        string getDueDate(); 
-        string getClassification();
+        std::string getDueDate(); 
+        std::string getClassification();
         bool getCompletion();
         int getProgress();   
 
 
-        void print(std::ostream &out);
-        void edit();  // specifications not complete
-        void deleteObj(int index, vector<Task> &temp); // changed method name because 'delete' is c++ key word
+        virtual void print(); //had this as an argument originally std::ostream &out
+        virtual void edit();  // specifications not complete
+        void deleteObj(int index2, TaskList &temp);
+
 };
 
 #endif
