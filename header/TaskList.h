244d5795669e674073c04f24bb5b26b6a337d21e
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-27 21:20:44 -0800

    updated include path + some changes

--- header/TaskList.h
+++ header/TaskList.h
@@ -4,17 +4,33 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "Task.h"
-#include "Goal.h"
+#include "../header/Task.h"
+#include "..header/Goal.h"
 
 using namespace std;
 
 class TaskList: public Goal{
     private:
         vector<Goal*> listObj;
+        std::string name;
+        bool lookInMain; //lets program know that user wants to add an existing task to the list
     public:
         ~TaskList(){}
-        TaskList():Goal(){}
+        TaskList():Goal(){
+            name = "default";
+        }
+        TaskList(string n):Goal(n){
+            name = n;
+        }
+        std::string getName(){
+            return name;
+        }
+        void setLookInMain(bool answer){
+            lookInMain = answer;
+        }
+        bool getLookInMain(){
+            return lookInMain;
+        }
         void print(); // had this as an argument originally std::ostream &out
         void edit();      // does this take arguments?
         void addTask(Goal*);
