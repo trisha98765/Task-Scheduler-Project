35cffed03f9dc1aca7cad19b572052efde9a7473
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-28 17:03:44 -0800

    Added delete function

--- header/TaskList.h
+++ header/TaskList.h
@@ -7,23 +7,28 @@
 #include "../header/Task.h"
 #include "..header/Goal.h"
 
-using namespace std;
-
 class TaskList: public Goal{
     private:
-        vector<Goal*> listObj;
+        std::vector<Goal*> listObj;
         std::string name;
         bool lookInMain; //lets program know that user wants to add an existing task to the list
+        bool deleted; //lets main know that list has been deleted
     public:
         ~TaskList(){}
         TaskList():Goal(){
             name = "default";
         }
-        TaskList(string n):Goal(n){
+        TaskList(std::string n):Goal(n){
             name = n;
         }
         std::string getName(){
             return name;
+        }
+        void setBool(bool input){
+            deleted =  input;
+        }
+        bool getBool(){
+            return deleted;
         }
         void setLookInMain(bool answer){
             lookInMain = answer;
@@ -34,10 +39,10 @@ class TaskList: public Goal{
         void print(); // had this as an argument originally std::ostream &out
         void edit();      // does this take arguments?
         void addTask(Goal*);
-        vector<Goal*>& getList(); //passed by reference
-        //void deleteObj(int index, TaskList &tempList);
-        Goal* findTask(string);
-        int findIndex(string);
+        std::vector<Goal*>& getList(); //passed by reference
+        void deleteObj();
+        Goal* findTask(std::string);
+        int findIndex(std::string);
 };
 
 #endif
