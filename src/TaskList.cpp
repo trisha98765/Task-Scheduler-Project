d886d79224fd193c25ba948f75d450442cd221ba
Melissa Hidalgo <mhida010@ucr.edu>
2022-02-28 18:19:29 -0800

    ostream fixes

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -16,16 +16,17 @@ void TaskList::print(std::ostream &out){
    
 }
 
-void TaskList::edit(){
+void TaskList::edit(std::ostream &out){
     int input = 0;
-    std::cout << "What would you like to do?" << std::endl;
-    std::cout << "1. Edit list name" << std::endl << std::cout << "2. Remove a task" << std::endl; //this prints a 1 before for some reason
-    std::cout << "3. Add a task" << std::endl;
+    out << "What would you like to do?" << std::endl;
+    out << "1. Edit list name" << std::endl;
+    out << "2. Remove a task" << std::endl; //this prints a 1 before for some reason
+    out << "3. Add a task" << std::endl;
     cin >>input;
 
     if(input == 1){
         std::string newName;
-        std::cout << "Please enter the new name" << std::endl;
+        out << "Please enter the new name" << std::endl;
         cin.ignore(); getline(cin, newName);
         this->setName(newName);
 
@@ -34,10 +35,10 @@ void TaskList::edit(){
 
     else if(input == 2){
         std::string taskName = "";
-        std::cout << "Enter the name of the task you want to remove: "; cin.ignore(); getline(cin, taskName); std::cout << std::endl;
+        out << "Enter the name of the task you want to remove: "; cin.ignore(); getline(cin, taskName); out << std::endl;
         Goal* tempTask = this->findTask(taskName);
         if(tempTask->getName() == ""){
-            std::cout << "Task not present" << std::endl;
+            out << "Task not present" << std::endl;
         }
         else{
             int tempInt = this->findIndex(taskName);
@@ -53,7 +54,7 @@ void TaskList::edit(){
     }
     else if(input == 3){
         int input2;
-        std::cout << "Do you want to 1. Add an existing task or 2. Add a new task to this list?" << std::endl;
+        out << "Do you want to 1. Add an existing task or 2. Add a new task to this list?" << std::endl;
         cin >>input2;
         
         if(input2 == 1){
@@ -64,13 +65,13 @@ void TaskList::edit(){
             int int1, int2, int3;
             bool bool1 = false; //set to false bc it's a new task
             cin.ignore();
-            std::cout << "Create a name: "; getline(std::cin, string1);
-            std::cout << std::endl << "Write a short description: "; getline(std::cin, string2);
-            std::cout << std::endl << "Set the priority level (1-10): "; cin >> int1;
-            std::cout << std::endl << "Classify your task: "; cin.ignore(); getline(cin, string3);
-            std::cout << std::endl << "How many days will this task last? "; cin >> int2;
-            std::cout << std::endl << "Create a due date in the form XX/YY/ZZ: "; cin.ignore(); getline(cin, string4);
-            std::cout << std::endl << "Enter a progress level (1-10): "; cin >> int3; std::cout << std::endl;
+            out << "Create a name: "; getline(std::cin, string1);
+            out << std::endl << "Write a short description: "; getline(std::cin, string2);
+            out << std::endl << "Set the priority level (1-10): "; cin >> int1;
+            out << std::endl << "Classify your task: "; cin.ignore(); getline(cin, string3);
+            out << std::endl << "How many days will this task last? "; cin >> int2;
+            out << std::endl << "Create a due date in the form XX/YY/ZZ: "; cin.ignore(); getline(cin, string4);
+            out << std::endl << "Enter a progress level (1-10): "; cin >> int3; out << std::endl;
             Goal *newTask = new Task(string1, string2, int1, int2, string4, string3, bool1, int3);
             this->addTask(newTask);
             this->setLookInMain(false);
@@ -80,18 +81,18 @@ void TaskList::edit(){
 void TaskList::addTask(Goal *newOne){
     listObj.push_back(newOne);
 }
-void TaskList::deleteObj() {
+void TaskList::deleteObj(std::ostream &out) {
     std::string input;
     // cin.ignore();
-    std::cout << "Would you like to delete this list? Y/N: ";
+    out << "Would you like to delete this list? Y/N: ";
     std::cin >> input;
-    std::cout  << std::endl;
+    out  << std::endl;
     if(input == "Y"){
-        std::cout << "List deleted. Undo? Y/N: ";
+        out << "List deleted. Undo? Y/N: ";
         cin >> input;
-        std::cout << std::endl;
+        out << std::endl;
         if(input == "Y"){
-            std::cout << "List restored" << std::endl;
+            out << "List restored" << std::endl;
         }
         else{
             this->setBool(true);        
