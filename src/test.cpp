25052439dc8921415403d6825f2778275583eaf1
trisha112 <tagra002@ucr.edu>
2022-03-01 23:22:16 -0800

    testing in progress

--- src/test.cpp
+++ src/test.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 
 static TaskList testList = TaskList("Test");
+static std::stringstream output;
 
 TEST(TaskConstructorTest, defaultConstructor){
     Task *test = new Task();
-   // EXPECT_EQ(test->getName(), "Homework");
+    EXPECT_EQ(test->Goal::getName(), "");
     EXPECT_EQ(test->getDescription(), "For CS 100");
     EXPECT_EQ(test->getPriority(), 1);
     EXPECT_EQ(test->getClassification(), "School");
@@ -21,7 +22,7 @@ TEST(TaskConstructorTest, defaultConstructor){
 
 TEST(TaskConstructorTest, overloadedConstructor){
     Task *test = new Task("Notes","For CS 100",3,3,"02/22/22","School",false,0);
-    // EXPECT_EQ(test->getName(), "Notes");
+    EXPECT_EQ(test->Goal::getName(), "Notes");
     EXPECT_EQ(test->getDescription(), "For CS 100");
     EXPECT_EQ(test->getPriority(), 3);
     EXPECT_EQ(test->getClassification(), "School");
@@ -34,8 +35,18 @@ TEST(TaskConstructorTest, overloadedConstructor){
 TEST(TaskMethodTest, addTask){
     Goal *task2 = new Task("Study Guide", "For CS 100", 1, 3,"03/11/22", "Winter 2022", false, 0);
     testList.addTask(task2);
-    testList.print(std::cout);
+    testList.print(output);
+    EXPECT_EQ(output.str(),"Test\nName: Notes\nDue: 03/19/22\nPriority: 2\nClassification: Winter 2022\nDuration: 10\nDescription: For CS 100\nCompleted? No\nProgress status: 5\n\nName: Study Guide\nDue: 03/11/22\nPriority: 1\nClassification: Winter 2022\nDuration: 3\nDescription: For CS 100\nCompleted? No\nProgress status: 0\n\n");
 }
+
+TEST(TaskMethodTest, editTask){ // test using user input 
+    Goal *task3 = new Task("Lab 3", "For CS 100", 1, 20,"03/01/22", "Winter 2022", false, 5);
+    testList.addTask(task3);
+       
+    testList.print(output);
+    EXPECT_EQ(output.str(),"");
+}
+
 
 int main(int argc, char **argv) {
     Goal *task1 = new Task("Notes", "For CS 100", 2, 10,"03/19/22", "Winter 2022", false, 5);
