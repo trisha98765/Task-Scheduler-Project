f4742ae5f6a6ac9dbcd666449474a1b338b3a05b
Evan Shimoniak <eshim009@ucr.edu>
2022-03-05 23:20:19 -0800

    Clarified date format in prompt
    Ambiguous as to whether date is American (MM/DD/YY) or European (DD/MM/YY)

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -60,7 +60,7 @@ void menu(int input,TaskList &unsorted, vector<TaskList> &allLists){
                 std::cout << std::endl << "Set the priority level (1-10): "; cin >> int1;
                 std::cout << std::endl << "Classify your task: "; cin.ignore(); getline(cin, string3);
                 std::cout << std::endl << "How many days will this task last? "; cin >> int2;
-                std::cout << std::endl << "Create a due date in the form XX/YY/ZZ: "; cin.ignore(); getline(cin, string4);
+                std::cout << std::endl << "Create a due date in the form MM/DD/YY: "; cin.ignore(); getline(cin, string4);
                 std::cout << std::endl << "Enter a progress level (1-10): "; cin >> int3; std::cout << std::endl;
                 Goal *newTask = new Task(string1, string2, int1, int2, string4, string3, bool1, int3);
                 unsorted.addTask(newTask);
