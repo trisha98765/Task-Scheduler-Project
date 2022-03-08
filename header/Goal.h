3f756c0069ecccdc3761b5cfe50c006a1492e8a4
Evan Shimoniak <eshim009@ucr.edu>
2022-03-08 15:59:50 -0800

    Moved Goal definitions out of header

--- header/Goal.h
+++ header/Goal.h
@@ -5,26 +5,15 @@
 #include <vector>
 #include <string>
 #include <ostream>
-#include "../header/TaskList.h"
-#include "../header/Task.h"
 
 class Goal{
     private:
         std::string name;
     public:
-        ~Goal(){ }
-        Goal(){
-            name = "";
-        }
-        Goal(std::string s){
-            name = s;
-        }
-        void setName(std::string s){
-            name = s;
-        }
-        std::string getName(){
-            return name;
-        }
+        Goal();
+        Goal(std::string s);
+        void setName(std::string s);
+        std::string getName();
         virtual void print(std::ostream &out) = 0;
         virtual void edit(std::ostream &out,int val,std::string update) = 0;      // does this take arguments?
         virtual void deleteObj(std::ostream &out) = 0;
